7d150223b448400a9c34946dd7aec28e8008fe8a
JJ Merelo <jjmerelo@gmail.com>
2017-11-04 19:45:27 +0100

    Procesando datos con R

--- scripts/cataluña.R
+++ scripts/cataluña.R
@@ -3,3 +3,7 @@ data.2016 <- read.table("data/Cataluña-2016.dat")
 data.2017 <- read.table("data/Cataluña-2017.dat")
 
 data <- data.frame(visitas.2016=data.2016$V1, visitas.2017=data.2017$V1, ratio=data.2017/data.2016)
+print(mean(data.2017$V1))
+print(mean(data.2016$V1))
+print(mean(data$visitas.2017))
+print(mean(data$visitas.2016))
