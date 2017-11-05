e8639b89ec21e92c87e696e0669bdc9dfd4bcb20
JJ Merelo <jjmerelo@gmail.com>
2017-11-05 10:36:51 +0100

    Añade nuevos datos

--- scripts/descarga-cataluña.sh
+++ scripts/descarga-cataluña.sh
@@ -5,4 +5,6 @@ api="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedi
 
 curl "$api/Catalu%C3%B1a/daily/2017090100/2017110100" | jq '.items[] | .views ' > Cataluña-2017.dat
 curl "$api/Catalu%C3%B1a/daily/2016090100/2016110100" | jq '.items[] | .views ' > Cataluña-2016.dat
+curl "$api/Catalu%C3%B1a/daily/2015090100/2015110100" | jq '.items[] | .views ' > Cataluña-2015.dat
+
 
